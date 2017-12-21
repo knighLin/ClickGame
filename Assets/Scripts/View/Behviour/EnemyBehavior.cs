00bd31e3b2a2696baa5f49731ecb6591681c451c
4419-3-3\user <85book@gmail.com>
2017-12-21 21:15:27 +0800

    Start Project

---
+++ Assets/Scripts/View/Behviour/EnemyBehavior.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]//確保執行此程式時，會自動添加該元件，但變數還是要自己設定
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(HealthComponent))]
+public class EnemyBehavior : MonoBehaviour {
+    private MeshFader meshFader;
+    private Animator animator;
+    private AudioSource audioSource;
+    private HealthComponent healthComponent;
+
+    [SerializeField]private AudioClip hurtClip;
+    [SerializeField]private AudioClip deadClip;
+ 
+    public bool IsDead
+    {
+        get
+        {
+            return healthComponent.Death;
+        }
+    }
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<HealthComponent>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(meshFader.FadeIn());
+    }
+
+
+    [ContextMenu("Test Execute")]
+    private void TestExecute()
+    {
+        StartCoroutine(Execute());
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            DoDamage(10);
+    }
+
+    #region Public Method
+    /// <summary>
+    /// 請呼叫我，開始敵人生命
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator Execute()
+    {
+        healthComponent.Init(100);
+        while(IsDead == false)//當還活著，死了跳出迴圈
+        {
+            yield return null;
+        }
+        animator.SetTrigger("die");
+        audioSource.clip = deadClip;
+        audioSource.Play();
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);//當前播放的animator裡的動畫片段的長度
+        yield return StartCoroutine(meshFader.FadeOut());//呼叫淡出
+    }
+    
+    
+    public void DoDamage(int attack)
+    {
+        healthComponent.Hurt(attack);
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/View/Component/HealthComponent.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthComponent : MonoBehaviour {
+
+    public Slider healthSlider;
+    private int currentHealth;//如果沒有設值，一開始是0
+    public float smooth = 5f;
+    public bool Death
+    {
+        get
+        {
+            return currentHealth <= healthSlider.minValue;//如果小於最小值Death回傳true
+        }
+    }
+
+    /*[ContextMenu("Test Value")]//測試
+    private void TestHurt()
+    {
+        Init(100);
+        Hurt(20);
+    }*/
+    public void Init(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = healthSlider.maxValue;
+        currentHealth = (int)healthSlider.maxValue;
+    }
+
+    public void Hurt(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = (int)Mathf.Max(healthSlider.minValue, currentHealth);//當當前血量小於最小值，只會顯示最小值，不會變負數。(Math.Max->誰大回傳誰)
+        
+    }
+	
+	
+	// Update is called once per frame
+	void Update () {
+        healthSlider.value = Mathf.Lerp(healthSlider.value, currentHealth, Time.deltaTime * smooth);
+
+	}
+}
